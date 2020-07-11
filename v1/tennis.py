4ec8f626eb082bc9e5997435345c87b0f848f55d
Mason.Lin <mason.lin1@hp.com>
2020-07-11 20:11:04 +0800

    pass

--- v1/tennis.py
+++ v1/tennis.py
@@ -2,8 +2,10 @@ class TennisGame(object):
     _firstPlayerScoreTimes = 0
 
     def Score(self):
-        if self._firstPlayerScoreTimes:
+        if self._firstPlayerScoreTimes == 1:
             return "Fifteen Love"
+        elif self._firstPlayerScoreTimes == 2:
+            return "Thirty Love"
         return "Love All"
 
     def FirstPlayerScore(self):
