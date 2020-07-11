679ee561df485d8cc52dc08c2ee2ed9ab3037455
Mason.Lin <mason.lin1@hp.com>
2020-07-11 20:10:17 +0800

    add fail test

--- v1/test_tennis.py
+++ v1/test_tennis.py
@@ -1,12 +1,22 @@
 from .tennis import TennisGame
 
+
 def test_Love_All():
     game = TennisGame()
     score = game.Score()
     assert score == "Love All"
+
 
 def test_fifteen_love():
     game = TennisGame()
     game.FirstPlayerScore()
     score = game.Score()
     assert score == "Fifteen Love"
+
+
+def test_thirty_love():
+    game = TennisGame()
+    game.FirstPlayerScore()
+    game.FirstPlayerScore()
+    score = game.Score()
+    assert score == "Thirty Love"
