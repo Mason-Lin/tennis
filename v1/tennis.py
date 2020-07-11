e9efa26b43c8244c3e849990308ca1252e119a67
Mason.Lin <mason.lin1@hp.com>
2020-07-11 20:50:54 +0800

    pass

--- v1/tennis.py
+++ v1/tennis.py
@@ -16,10 +16,29 @@ class TennisGame(object):
     def secondPlayerScore(self, score):
         self._secondPlayerScore = score
 
+    @property
+    def firstPlayerName(self):
+        return self._firstPlayerName
+
+    @firstPlayerName.setter
+    def firstPlayerName(self, name):
+        self._firstPlayerName = name
+
+    @property
+    def secondPlayerName(self):
+        return self._secondPlayerName
+
+    @secondPlayerName.setter
+    def secondPlayerName(self, name):
+        self._secondPlayerName = name
+
     def __init__(self):
         super().__init__()
         self.firstPlayerScore = 0
         self.secondPlayerScore = 0
+        self.firstPlayerName = "Mason"
+        self.secondPlayerName = "Rina"
+
 
     def Score(self):
         score_lookup = {
@@ -35,6 +54,8 @@ class TennisGame(object):
                     return "Deuce"
                 else:
                     return f"{score_lookup[str(self.firstPlayerScore)]} All"
+            elif (self.firstPlayerScore, self.secondPlayerScore) == (4, 3):
+                return f"{self.firstPlayerName} Adv"
             return f"{score_lookup[str(self.firstPlayerScore)]} {score_lookup[str(self.secondPlayerScore)]}"
         else:
             return "Love All"
