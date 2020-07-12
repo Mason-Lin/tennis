3ee5444020b848de528a3e8655fbd6315bdadcff
Mason.Lin <mason.lin1@hp.com>
2020-07-12 13:38:07 +0800

    finish

--- v2/tennis.py
+++ v2/tennis.py
@@ -27,15 +27,15 @@ class TennisGame():
             "0": "Love",
             "1": "Fifteen",
             "2": "Thirty",
-            "3": "Forty"
+            "3": "Forty",
+            "4": "Adv",
+            "5": "Win"
         }
 
         if self.is_same_score():
-            return self.get_high_same_score_result() if self.is_first_score_high_than_forty() else self.get_low_same_score_result()
+            return self.get_high_same_score_result() if self.is_both_score_high_than_forty() else self.get_low_same_score_result()
         else:
             return self.get_high_diff_score_result() if self.is_both_score_high_than_forty() else self.get_low_diff_score_result()
-
-
 
     def get_low_same_score_result(self):
         return f"{self.score_lookup[str(self.first_player_score)]}-All"
@@ -53,13 +53,10 @@ class TennisGame():
         return self.first_player_name if self.first_player_score > self.second_player_score else self.second_player_name
 
     def get_adv_statue_by_max_score(self):
-        return {"4": "Adv", "5": "Win"}[str(max(self.first_player_score, self.second_player_score))]
+        return self.score_lookup[str(max(self.first_player_score, self.second_player_score))]
 
     def is_both_score_high_than_forty(self):
         return min(self.first_player_score, self.second_player_score) >= 3
-
-    def is_first_score_high_than_forty(self):
-        return self.first_player_score >= 3
 
     def is_same_score(self):
         return self.first_player_score == self.second_player_score
