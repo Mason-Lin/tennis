446cea8f0c64de7318803e1310f6d1065a73fffd
Mason.Lin <mason.lin1@hp.com>
2020-07-19 21:04:46 +0800

    fail test

--- v3/test_tennis.py
+++ v3/test_tennis.py
@@ -4,3 +4,9 @@ def test_fifteen_love():
     game = TennisGame()
     game.first_player_score()
     assert game.score() == "Fifteen-Love"
+
+def test_thirty_love():
+    game = TennisGame()
+    game.first_player_score()
+    game.first_player_score()
+    assert game.score() == "Thirty-Love"
