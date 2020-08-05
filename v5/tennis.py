0621cc5f1cdd45c1e6aa89fee7d6a72b38692601
Mason.Lin <mason.lin1@hp.com>
2020-08-06 00:14:05 +0800

    refactoring

--- v5/tennis.py
+++ v5/tennis.py
@@ -30,13 +30,6 @@ class TennisGame:
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
-        # SCORE_LOOK(P = {     #     "0": "Lov),
-        #     "1": "Fifteen",
-        #     "2": "Thirty",
-        #     "3": "Forty",
-        #     "4": "Adv",
-        #     "5": "Win",
-        # }
 
     def is_going_to_win(self):
         return min(self.player1.get_score(), self.player2.get_score()) >= 3
@@ -48,9 +41,19 @@ class TennisGame:
             else self.player2
         )
 
+    def is_same_score(self):
+        return self.player1.get_score() == self.player2.get_score()
+
     def score(self):
         logging.debug(self.player1.get_score)
-        if self.player1.get_score() != self.player2.get_score():
+        if self.is_same_score():
+            if self.is_going_to_win():
+                return "Deuce"
+            else:
+                return "{}-{}".format(
+                    SCORE_LOOKUP(self.player1.get_score()).name, "All"
+                )
+        else:
             if self.is_going_to_win():
                 winner = self.get_winner()
                 return "{} {}".format(
@@ -60,12 +63,4 @@ class TennisGame:
                 return "{}-{}".format(
                     SCORE_LOOKUP(self.player1.get_score()).name,
                     SCORE_LOOKUP(self.player2.get_score()).name,
-                )
-        else:
-            # self.player1.get_score() == self.player2.get_score():
-            if self.is_going_to_win():
-                return "Deuce"
-            else:
-                return "{}-{}".format(
-                    SCORE_LOOKUP(self.player1.get_score()).name, "All"
                 )
