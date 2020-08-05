87ea239269fb845dae5229982e99a7e6ed145fce
Mason.Lin <mason.lin1@hp.com>
2020-08-05 23:12:26 +0800

    add fail

--- v5/test_tennis.py
+++ v5/test_tennis.py
@@ -59,3 +59,7 @@ def test_thirty_all(game, player1, player2):
     win_multiple_time(player2, 2)
     assert game.score() == "Thirty-All"
 
+def test_deuce(game, player1, player2):
+    win_multiple_time(player1, 3)
+    win_multiple_time(player2, 3)
+    assert game.score() == "Deuce"
