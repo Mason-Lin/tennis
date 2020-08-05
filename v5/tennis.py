cb43547dffd36c17da00ad2a8b686ef6819b5c12
Mason.Lin <mason.lin1@hp.com>
2020-08-05 23:02:29 +0800

    pass

--- v5/tennis.py
+++ v5/tennis.py
@@ -15,11 +15,11 @@ class TennisGame:
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
-        self.score_lookup = {"0": "Love", "1": "Fifteen", "2": "Thirty"}
+        self.score_lookup = {"0": "Love", "1": "Fifteen", "2": "Thirty", "3": "Forty"}
 
     def score(self):
         logging.debug(self.player1.get_score)
-        if self.player1.get_score() <= 2 and self.player2.get_score() == 0:
+        if self.player1.get_score() != self.player2.get_score():
             return "{}-{}".format(
                 self.score_lookup[str(self.player1.get_score())],
                 self.score_lookup[str(self.player2.get_score())],
