24da2705266ffcfac93580ad7370b4356a2d223c
Mason.Lin <mason.lin1@hp.com>
2020-08-05 22:56:50 +0800

    refactoring

--- v5/tennis.py
+++ v5/tennis.py
@@ -15,15 +15,20 @@ class TennisGame:
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
+        self.score_lookup = {
+            "0": "Love",
+            "1": "Fifteen",
+            "2": "Thirty"
+        }
 
     def first_player_get_score(self):
         self.player1.win_score()
 
+
+
     def score(self):
         logging.debug(self.player1.get_score)
-        if self.player1.get_score() == 1 and self.player2.get_score() == 0:
-            return "Fifteen-Love"
-        elif self.player1.get_score() == 2 and self.player2.get_score() == 0:
-            return "Thirty-Love"
+        if self.player1.get_score() <= 2 and self.player2.get_score() == 0:
+            return "{}-{}".format(self.score_lookup[str(self.player1.get_score())], self.score_lookup[str(self.player2.get_score())])
         else:
-            return "xxxx"
+            raise NotImplementedError
