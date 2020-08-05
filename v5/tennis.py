029334ad6791a8ea076a333dcea828f62d4a5ee7
Mason.Lin <mason.lin1@hp.com>
2020-08-05 23:00:44 +0800

    refactoring

--- v5/tennis.py
+++ v5/tennis.py
@@ -1,5 +1,6 @@
 import logging
 
+
 class Player:
     def __init__(self):
         self.score = 0
@@ -10,25 +11,18 @@ class Player:
     def get_score(self):
         return self.score
 
-
 class TennisGame:
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
-        self.score_lookup = {
-            "0": "Love",
-            "1": "Fifteen",
-            "2": "Thirty"
-        }
-
-    def first_player_get_score(self):
-        self.player1.win_score()
-
-
+        self.score_lookup = {"0": "Love", "1": "Fifteen", "2": "Thirty"}
 
     def score(self):
         logging.debug(self.player1.get_score)
         if self.player1.get_score() <= 2 and self.player2.get_score() == 0:
-            return "{}-{}".format(self.score_lookup[str(self.player1.get_score())], self.score_lookup[str(self.player2.get_score())])
+            return "{}-{}".format(
+                self.score_lookup[str(self.player1.get_score())],
+                self.score_lookup[str(self.player2.get_score())],
+            )
         else:
             raise NotImplementedError
--- v5/test_tennis.py
+++ v5/test_tennis.py
@@ -2,12 +2,16 @@ import pytest
 
 from .tennis import TennisGame, Player
 
+def win_multiple_time(who, times):
+    for _ in range(times):
+        who.win_score()
 
 def test_fifteen_love():
     player1 = Player()
     player2 = Player()
     game = TennisGame(player1, player2)
-    game.first_player_get_score()
+    win_multiple_time(player1, 1)
+    win_multiple_time(player2, 0)
     assert game.score() == "Fifteen-Love"
 
 
@@ -15,6 +19,6 @@ def test_thirty_love():
     player1 = Player()
     player2 = Player()
     game = TennisGame(player1, player2)
-    game.first_player_get_score()
-    game.first_player_get_score()
+    win_multiple_time(player1, 2)
+    win_multiple_time(player2, 0)
     assert game.score() == "Thirty-Love"
